ac1601c3957d35dd9f90cc386ccfafb6b783e18d
yonathanseleshi <yonathan.seleshi@gmail.com>
2018-08-03 18:47:12 -0700

    Fixed Update and GetAll Methods

--- QuizAppBackend/Controllers/QuestionsController.cs
+++ QuizAppBackend/Controllers/QuestionsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using QuizAppBackend.Models;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,33 +20,52 @@ namespace QuizAppBackend.Controllers
 
         }
 
-        // GET: api/values
+        // GET: api/Questions
         [HttpGet]
         public IEnumerable<Question> Get()
         {
-            return new Question[] { };
+
+            return _context.Questions;
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<IActionResult> Get(int id)
         {
-            return "value";
+
+
+            var question = await _context.Questions.SingleOrDefaultAsync(x => x.ID == id);
+
+            if (question == null)
+            {
+
+                NotFound();
+            }
+
+            return Ok(question);
         }
 
         // POST api/values
         [HttpPost]
-        public async void Post([FromBody]Question question)
+        public async Task<IActionResult> Post([FromBody]Question question)
         {
 
             await _context.Questions.AddAsync(question);
             await _context.SaveChangesAsync();
+            return Ok(question);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public async Task<IActionResult> Put(int id, [FromBody]Question question)
         {
+
+            if (id != question.ID) return BadRequest();
+
+            _context.Entry(question).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return Ok(question);
         }
 
         // DELETE api/values/5
--- QuizAppBackend/Models/Question.cs
+++ QuizAppBackend/Models/Question.cs
@@ -10,5 +10,10 @@ namespace QuizAppBackend.Models
 
         public int ID { get; set; }
         public string Text { get; set; }
+        public string CorrectAnswer { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+
     }
 }
